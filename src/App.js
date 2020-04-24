c6966608fbc3e65e457217a3ef8d95fedc521294
Tori <aretee64@gmail.com>
2020-04-24 05:37:43 -0600

    signin and signout for google

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Homepage from './pages/Homepage/Homepage.component'
 import './App.css';
 import {Route, Switch} from 'react-router-dom';
 import shopPage from './pages/Shopping/Shop.component'
 import Header from './components/Header/Header.component'
 import LoginPage from './pages/Login/Login.component';
-import RegisterPage from './pages/Login/Register.component'
+import RegisterPage from './pages/Login/Register.component';
+import {auth} from './firebase/firebase';
 
 
- const constantName = () => (
-   <div>
- test comp
-   </div>
- )
- //Switch is going to really restrict what renders based on the path
-function App() {
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser : null
+    }
+  }
+
+  unsubscribeFromAuth = null
+  // assigning the state of currentUser object to the User that gets logged in
+  //whenever changes happen, sends the user updated
+  //always open subscription as long as component is mounted
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user})
+      console.log(user)
+    })
+  }
+
+  // closes the subscription 
+  componentWillUnmount() {
+    this.unsubscribeFromAuth()
+  }
+
+
+
+ render() {
   return (
     <div> 
-      <Header />
-      <Switch> 
+      <Header currentUser={this.state.currentUser}/>
+      {/*  Switch is going to really restrict what renders based on the path */}
+      <Switch>  
       <Route exact path='/' component={Homepage} />
       <Route exact path='/shop' component={shopPage} />
       <Route exact path='/login' component={LoginPage} />
@@ -30,6 +53,7 @@ function App() {
       </Switch>
     </div>
   );
+ }
 }
 
 export default App;
--- src/components/Header/Header.component.jsx
+++ src/components/Header/Header.component.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
@@ -8,17 +8,19 @@ import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import { makeStyles } from '@material-ui/core/styles';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import { auth } from '../../firebase/firebase';
+import LocalDiningIcon from '@material-ui/icons/LocalDining';
 
 
 const useStyles = makeStyles((theme) => ({
     root: {
-      flexGrow: 1,
+        flexGrow: 1,
     },
     menuButton: {
-      marginRight: theme.spacing(2),
+        marginRight: theme.spacing(2),
     },
     title: {
-      flexGrow: 1,
+        flexGrow: 1,
     },
     appbar: {
         backgroundColor: 'darkorange'
@@ -31,28 +33,39 @@ const useStyles = makeStyles((theme) => ({
         //     color: '#FFF'
         // }
     }
-  }));
+}));
 
 // HEADER with links
-const Header = () => {
+const Header = ({ currentUser }) => {
     const classes = useStyles();
     return (
         <div className={classes.root}>
-        <AppBar position="static" className={classes.appbar}>
-          <Toolbar>
-            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" className={classes.title}>
-            Anything For Food
+            <AppBar position="static" className={classes.appbar}>
+                <Toolbar>
+                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                        <Link to='/'>
+                        <LocalDiningIcon />
+                        </Link>
+                    </IconButton>
+                    <Typography variant="h6" className={classes.title}>
+                        Anything For Food
             </Typography>
-            <Link className={classes.button} to ='/login'>
-            <Button className={classes.button} color="inherit">Login</Button>
-            </Link>
-            <ShoppingCartIcon />
-          </Toolbar>
-        </AppBar>
-      </div>
+                    <Link className={classes.button} to='/shop'>
+                        <Button className={classes.button} color="inherit">Order</Button>
+                    </Link>
+                    {/* checks if there is a current user to display login/logout */}
+                    {
+                        currentUser ?
+                            <Button onClick={() => auth.signOut()} className={classes.button} color="inherit">Logout</Button>
+                            :
+                            <Link to='/login' className={classes.button}>
+                                <Button className={classes.button} color="inherit">Login</Button>
+                            </Link>
+                    }
+                    <ShoppingCartIcon />
+                </Toolbar>
+            </AppBar>
+        </div>
     )
 }
 
